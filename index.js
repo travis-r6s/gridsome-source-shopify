983a448deef11a787c445ee7e8913b19b378222b
Travis Reynolds <travis@cirruscreative.co.uk>
2019-05-11 16:40:30 +0100

    Add collections queries, add referencing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const camelCase = require('camelcase')
 const { createClient } = require('./client')
-const { PRODUCTS_QUERY } = require('./queries')
+const { COLLECTIONS_QUERY, PRODUCTS_QUERY, PRODUCT_TYPES_QUERY } = require('./queries')
 
 // Node prefix
 const TYPE_PREFIX = 'Shopify'
@@ -37,7 +37,8 @@ class ShopifySource {
     this.shopify = createClient(options)
 
     this.routes = {
-      products: '/products/:slug'
+      products: '/products/:slug',
+      collections: '/collections/:slug'
     }
 
     api.loadSource(async store => {
@@ -45,16 +46,57 @@ class ShopifySource {
 
       console.log(`Loading data from ${options.storeUrl}`)
 
-      await this.getProductTypes(store)
+      // await this.getProductTypes(store)
+      await this.getCollections(store)
       await this.getProducts(store)
     })
   }
 
-  async getProductTypes (store) {
+  // async getProductTypes (store) {
+  //   const { productTypes: { edges: data } } = await this.shopify.request(PRODUCT_TYPES_QUERY, { first: this.options.perPage })
 
+  //   data.forEach(({ node: type }) => {
+  //     store.addContentType({
+  //       typeName: this.createTypeName(PRODUCT_TYPE),
+  //       route: `types/:slug`
+  //     })
+  //   })
+  // }
+
+  async getCollections (store) {
+    const { getContentType, createReference } = store
+
+    const PRODUCT_TYPE_NAME = this.createTypeName(PRODUCT)
+
+    const { collections: { edges: data } } = await this.shopify.request(COLLECTIONS_QUERY, { first: this.options.perPage })
+
+    const collections = store.addContentType({
+      typeName: this.createTypeName(COLLECTION),
+      route: this.routes.collections
+    })
+
+    data.forEach(({ node: collection }) => {
+      const products = collection.products.edges.map(({ node: product }) => createReference(PRODUCT_TYPE_NAME, product.id))
+      collections.addNode({
+        id: collection.id,
+        description: collection.description,
+        descriptionHtml: collection.descriptionHtml,
+        handle: collection.handle,
+        slug: collection.handle,
+        image: collection.image,
+        title: collection.title,
+        updatedAt: collection.updatedAt,
+        products
+      })
+    })
   }
 
+
   async getProducts (store) {
+    const { getContentType, createReference } = store
+
+    const COLLECTION_TYPE_NAME = this.createTypeName(COLLECTION)
+
     const { products: { edges: data } } = await this.shopify.request(PRODUCTS_QUERY, { first: this.options.perPage })
 
     const products = store.addContentType({
@@ -63,9 +105,11 @@ class ShopifySource {
     })
 
     data.forEach(({ node: product }) => {
+      const collections = product.collections.edges.map(({ node: collection }) => createReference(COLLECTION_TYPE_NAME, collection.id))
       products.addNode({
         id: product.id,
         createdAt: product.createdAt,
+        collections: collections,
         description: product.description,
         descriptionHtml: product.descriptionHtml,
         slug: product.handle,
@@ -75,6 +119,7 @@ class ShopifySource {
         options: product.options,
         priceRange: product.priceRange,
         product: product.productType,
+        productType: product.productType,
         published: product.publishedAt,
         tags: product.tags,
         title: product.title,
