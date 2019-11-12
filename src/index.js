bd7928938c17f8c6fec7d152eb4cca0a43a1958f
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-12 21:07:38 +0000

    update to typename

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import camelCase from 'camelcase'
 import { createClient, queryAll } from './client'
-import { COLLECTIONS_QUERY, PRODUCTS_QUERY, PRODUCT_TYPES_QUERY } from './queries'
+import { COLLECTIONS_QUERY, PRODUCTS_QUERY, PRODUCT_TYPES_QUERY, ARTICLES_QUERY, BLOGS_QUERY, SHOP_POLICIES_QUERY } from './queries'
 
 // Node prefix
 const TYPE_PREFIX = 'Shopify'
@@ -10,8 +10,6 @@ const ARTICLE = 'Article'
 const BLOG = 'Blog'
 const COLLECTION = 'Collection'
 const PRODUCT = 'Product'
-const PRODUCT_OPTION = 'ProductOption'
-const PRODUCT_VARIANT = 'ProductVariant'
 const SHOP_POLICY = 'ShopPolicy'
 const PRODUCT_TYPE = 'ProductType'
 
@@ -37,19 +35,19 @@ class ShopifySource {
     this.shopify = createClient(options)
 
     api.loadSource(async store => {
-      this.store = store
-
       console.log(`Loading data from ${options.storeUrl}`)
 
       await this.getProductTypes(store)
       await this.getCollections(store)
       await this.getProducts(store)
+      await this.getArticles(store)
+      await this.getBlogs(store)
     })
   }
 
   async getProductTypes (store) {
-    const PRODUCT_TYPE_TYPE_NAME = this.createTypeName(PRODUCT_TYPE)
-    const productTypeStore = store.addCollection({ typeName: PRODUCT_TYPE_TYPE_NAME })
+    const PRODUCT_TYPE_TYPENAME = this.createTypeName(PRODUCT_TYPE)
+    const productTypeStore = store.addCollection({ typeName: PRODUCT_TYPE_TYPENAME })
 
     const allProductTypes = await queryAll(this.shopify, PRODUCT_TYPES_QUERY, this.options.first)
 
@@ -61,14 +59,14 @@ class ShopifySource {
   async getCollections (store) {
     const { createReference } = store
 
-    const PRODUCT_TYPE_NAME = this.createTypeName(PRODUCT)
-    const COLLECTION_TYPE_NAME = this.createTypeName(COLLECTION)
-    const collectionStore = store.addCollection({ typeName: COLLECTION_TYPE_NAME })
+    const PRODUCT_TYPENAME = this.createTypeName(PRODUCT)
+    const COLLECTION_TYPENAME = this.createTypeName(COLLECTION)
+    const collectionStore = store.addCollection({ typeName: COLLECTION_TYPENAME })
 
     const allCollections = await queryAll(this.shopify, COLLECTIONS_QUERY, this.options.first)
 
     for (const collection of allCollections) {
-      const products = collection.products.edges.map(({ node: product }) => createReference(PRODUCT_TYPE_NAME, product.id))
+      const products = collection.products.edges.map(({ node: product }) => createReference(PRODUCT_TYPENAME, product.id))
       collectionStore.addNode({
         ...collection,
         products
@@ -79,14 +77,14 @@ class ShopifySource {
   async getProducts (store) {
     const { createReference } = store
 
-    const PRODUCT_TYPE_NAME = this.createTypeName(PRODUCT)
-    const COLLECTION_TYPE_NAME = this.createTypeName(COLLECTION)
-    const productStore = store.addCollection({ typeName: PRODUCT_TYPE_NAME })
+    const PRODUCT_TYPENAME = this.createTypeName(PRODUCT)
+    const COLLECTION_TYPENAME = this.createTypeName(COLLECTION)
+    const productStore = store.addCollection({ typeName: PRODUCT_TYPENAME })
 
     const allProducts = await queryAll(this.shopify, PRODUCTS_QUERY, this.options.first)
 
     for (const product of allProducts) {
-      const collections = product.collections.edges.map(({ node: collection }) => createReference(COLLECTION_TYPE_NAME, collection.id))
+      const collections = product.collections.edges.map(({ node: collection }) => createReference(COLLECTION_TYPENAME, collection.id))
       productStore.addNode({
         ...product,
         collections: collections,
