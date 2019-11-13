9c132119e6b6223d08e005417a811ba5bbce97d7
Travis Reynolds <travis@cirruscreative.co.uk>
2019-11-13 21:09:56 +0000

    add gridsome server file to create ShopifyImage schema type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import camelCase from 'camelcase'
 import { createClient, queryAll } from './client'
 import { COLLECTIONS_QUERY, PRODUCTS_QUERY, PRODUCT_TYPES_QUERY, ARTICLES_QUERY, BLOGS_QUERY, PAGES_QUERY } from './queries'
-
 // Node prefix
 const TYPE_PREFIX = 'Shopify'
 
@@ -35,6 +34,62 @@ class ShopifySource {
 
     this.shopify = createClient(options)
 
+    // Create custom schema type for ShopifyImage
+    api.loadSource(({ addSchemaTypes, schema, addSchemaResolvers }) => {
+      const IMAGE_TYPENAME = this.createTypeName(IMAGE)
+      addSchemaTypes([
+        schema.createEnumType({
+          name: `${IMAGE_TYPENAME}CropMode`,
+          values: {
+            CENTER: {},
+            TOP: {},
+            LEFT: {},
+            BOTTOM: {},
+            RIGHT: {}
+          }
+        }),
+        schema.createObjectType({
+          name: IMAGE_TYPENAME,
+          interfaces: ['Node'],
+          fields: {
+            altText: 'String',
+            originalSrc: 'String',
+            transformedSrc: 'String'
+          }
+        })
+      ])
+      addSchemaResolvers({
+        [ IMAGE_TYPENAME ]: {
+          transformedSrc: {
+            args: {
+              maxWidth: 'Int',
+              maxHeight: 'Int',
+              crop: `${IMAGE_TYPENAME}CropMode`,
+              scale: 'Int'
+            },
+            resolve ({ originalSrc }, { maxHeight, maxWidth, crop, scale }) {
+              // Create the transform for Shopif CDN
+              const dot = originalSrc.lastIndexOf('.')
+              const path = originalSrc.slice(0, dot)
+              const ext = originalSrc.slice(dot)
+              const transforms = []
+
+              if (maxWidth && maxHeight) transforms.push(`_${maxWidth}x${maxHeight}`)
+              else if (maxWidth) transforms.push(`_${maxWidth}x`)
+              else if (maxHeight) transforms.push(`_x${maxHeight}`)
+
+              if (crop && (maxWidth || maxHeight)) transforms.push(`_crop_${crop.toLowerCase()}`)
+              if (scale && scale !== 1) transforms.push(`@${scale}x`)
+
+              const transformedSrc = `${path}${transforms.join('')}${ext}`
+              return transformedSrc
+            }
+          }
+        }
+      })
+    })
+
+    // Load data into store
     api.loadSource(async actions => {
       console.log(`Loading data from ${options.storeUrl}`)
 
